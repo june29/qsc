ad411d9ecb12295f1e1c34a67cc497fe226aa2a9
june29 <june29x@gmail.com>
2008-09-02 02:23:34 +0900

    footer に Google Analytics のコードが正しく埋め込まれていなかった問題を修正した

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja">
 
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title>
-<?php
+<title><?php
 if(is_404()): echo 'Not Found - '; endif;
 if(is_category()): echo single_cat_title().' カテゴリのエントリ'; endif;
 if(is_tag()): echo single_tag_title().' タグを含むエントリ'; endif;
